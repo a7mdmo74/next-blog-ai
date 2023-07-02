fe69adfba03b542a50c366625857d97afaf5113d
a7mdmo74 <a7mdmo74@gmail.com>
2023-07-03 00:11:14 +0300

    update some error

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -18,7 +18,7 @@ export async function POST(request: Request, response: any) {
           {
             role: 'user',
             // content: `Create small blog post with html tags based on this title: ${title}`,
-            content: `Create 3 line blog post with html tags based on this title: ${title}`,
+            content: `Create 10 line blog post with html tags based on this title: ${title}`,
           },
           {
             role: 'system',
