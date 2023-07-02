67e0f77824cd9b36398ab589d91a240f3fd9d27d
a7mdmo74 <a7mdmo74@gmail.com>
2023-07-03 00:06:05 +0300

    first commit

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
+export { default as Trending } from './Trending';
+export { default as Tech } from './Tech';
+export { default as Travel } from './Travel';
+export { default as Other } from './Other';
+export { default as Subscribe } from './Subscribe';
+export { default as Sidebar } from './Sidebar';
