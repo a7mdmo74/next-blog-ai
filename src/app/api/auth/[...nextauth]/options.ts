619d035784c102b18d637a7b601244c5646cfca1
a7mdmo74 <a7mdmo74@gmail.com>
2023-07-03 12:38:18 +0300

    remove github token from commit history

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,4 @@
 import type { NextAuthOptions } from 'next-auth';
-import GitHubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const options: NextAuthOptions = {
