b3da4a07be774c186562afa32e69413a401089f8
a7mdmo74 <a7mdmo74@gmail.com>
2023-07-03 12:14:57 +0300

    solve api error

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -11,23 +11,22 @@ export async function POST(request: Request, response: any) {
   try {
     const { title, role } = await request.json();
 
-    const aiResponse: AxiosResponse<CreateChatCompletionResponse, any> =
-      await openai.createChatCompletion({
-        model: 'gpt-3.5-turbo',
-        messages: [
-          {
-            role: 'user',
-            // content: `Create small blog post with html tags based on this title: ${title}`,
-            content: `Create small blog post with html tags based on this title: ${title}`,
-          },
-          {
-            role: 'system',
-            content: `${
-              role || 'I am a helpful assistant'
-            }. Write with html tags.`,
-          },
-        ],
-      });
+    const aiResponse: any = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'user',
+          // content: `Create small blog post with html tags based on this title: ${title}`,
+          content: `Create small blog post with html tags based on this title: ${title}`,
+        },
+        {
+          role: 'system',
+          content: `${
+            role || 'I am a helpful assistant'
+          }. Write with html tags.`,
+        },
+      ],
+    });
 
     // response.revalidate("/api/posts")
     return NextResponse.json(
